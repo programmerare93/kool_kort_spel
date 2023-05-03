9a04274104576c05c41e1e9c7b580511ca9f081e
Oskar Björk <oskar.bjork@skola.taby.se>
2023-05-03 15:05:51 +0200

    SÄLLA

--- script.js
+++ script.js
@@ -1,80 +1,79 @@
 // TODO: Input där användaren anger antal högar och sedan väljer fördelning vid början
 
+// OLO
+
 function createPiles(deckSize) {
-	let piles = [];
+  let piles = [];
 
-	while (deckSize > 0) {
-		let pileSize = Math.floor(Math.random() * deckSize) + 1;
-		piles.push(pileSize);
-		deckSize -= pileSize;
-	}
+  while (deckSize > 0) {
+    let pileSize = Math.floor(Math.random() * deckSize) + 1;
+    piles.push(pileSize);
+    deckSize -= pileSize;
+  }
 
-	return piles
+  return piles;
 }
 
 function sortPiles(piles) {
-	piles.sort().reverse();
+  piles.sort().reverse();
 
-	// Tar bort alla tomma högar
-	let index = 0;
-	for (let pile of piles) {
-		if (pile === 0) {
-			piles.splice(index, 1);
-		}
-		index++;
-	}
+  // Tar bort alla tomma högar
+  let index = 0;
+  for (let pile of piles) {
+    if (pile === 0) {
+      piles.splice(index, 1);
+    }
+    index++;
+  }
 }
 
 function updatePiles(piles) {
-	let newPileLength = 0;
+  let newPileLength = 0;
 
-	let index = 0;
-	for (let pile of piles) {
-		if (pile === 0) {
-			continue;
-		}
-		newPileLength++;
-		piles[index] = --pile;
-		++index;
-	}
-	piles.push(newPileLength);
+  let index = 0;
+  for (let pile of piles) {
+    if (pile === 0) {
+      continue;
+    }
+    newPileLength++;
+    piles[index] = --pile;
+    ++index;
+  }
+  piles.push(newPileLength);
 }
 
 function hasWon(pilesArray) {
-	let localPilesArray = pilesArray.map((x) => x); // Kopia
+  let localPilesArray = pilesArray.map((x) => x); // Kopia
 
-	let pilesLeft = pilesArray.length();
-	while (pilesLeft > 0) {
-
-	}
+  let pilesLeft = pilesArray.length();
+  while (pilesLeft > 0) {}
 }
 
 function play() {
-	let piles = createPiles(10);
-	sortPiles(piles);
-	for (let i of piles) {
-		console.log(i);
-	}
+  let piles = createPiles(10);
+  sortPiles(piles);
+  for (let i of piles) {
+    console.log(i);
+  }
 
-	let pileArchive = new Array();
-	pileArchive.push(piles);
+  let pileArchive = new Array();
+  pileArchive.push(piles);
 
+  let isRunning = true;
+  let i = 0;
+  while (isRunning) {
+    console.log("New: ");
+    updatePiles(piles);
+    sortPiles(piles);
+    for (let i of piles) {
+      console.log(i);
+    }
 
-	let isRunning = true;
-	let i = 0;
-	while (isRunning) {
-		console.log("New: ");
-		updatePiles(piles);
-		sortPiles(piles);
-		for (let i of piles) {
-			console.log(i);
-		}
-
-		pileArchive.push(piles);
-		console.log(pileArchive);
-		//isRunning = hasWon(pileArchive);
-		isRunning = false;
-	}
+    pileArchive.push(piles);
+    console.log(pileArchive);
+    //isRunning = hasWon(pileArchive);
+    isRunning = false;
+  }
 }
 
 play();
