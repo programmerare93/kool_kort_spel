d0cc7864ef6553461311b3a13f8b4580e063d0f9
Simon Rehn <quesadilla.guerilla@protonmail.com>
2023-05-26 12:52:24 +0200

    Kommentarer

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // Våra html element
-
 const newPilesBtn = document.querySelector(".new--piles");
 const pilesContainer = document.querySelector(".piles--container");
 
@@ -9,13 +8,12 @@ const pilesContainer = document.querySelector(".piles--container");
 
 // Skapar de första högarna
 function createPiles() {
-  // NOTE: Input säkerhet?
   let piles = []; // Representerar en kombination av högar
 
   const deckSize = prompt("Ange hur många kort du vill ha: ");
   const numOfPiles = prompt("Ange hur många högar av kort du vill ha: ");
+  // Ser till så att man inte kan ange fler högar än kort
   if (Number(numOfPiles) > Number(deckSize)) {
-    // Ser till så att man inte kan ange fler högar än kort
     alert("Du angav fler kort högar än antal kort");
     piles = createPiles(deckSize);
     return piles;
@@ -45,8 +43,8 @@ function createPiles() {
     piles.push(input); // Varje hög representeras av ett heltal
   }
 
+  // Ifall användaren inte fördelade alla korten så läggs resten i en egen hög
   if (remainingNumOfCards > 0) {
-    // Ifall användaren inte fördelade alla korten så läggs resten i en egen hög
     alert(
       `Du fördelade inte all korten så resten (${remainingNumOfCards} st) kommer att automatiskt hamna i sin egna hög`
     );
@@ -56,8 +54,8 @@ function createPiles() {
   return piles;
 }
 
+// Skapar html elementen för att visa högarna grafiskt
 function createPileContainers(piles) {
-  // Skapar html elementen för att visa högarna grafiskt
   for (let pile of piles) {
     pilesContainer.innerHTML += `<p>${pile}</p>`; // Visar med ett heltal för användaren hur många kort som finns i högen
     for (let i = 0; i < pile; i++) {
@@ -68,16 +66,16 @@ function createPileContainers(piles) {
   }
 }
 
+// Sorterar högarna i fallande ordning
 function sortPiles(piles) {
-  // Sorterar högarna i fallande ordning
   return piles
     .sort()
     .reverse()
     .filter((pile) => pile !== 0);
 }
 
+// Tar bort ett kort från varje hög och lägger ihop dem i en ny hög
 function updatePiles(piles) {
-  // Tar bort ett kort från varje hög och lägger ihop dem i en ny hög
   let newPile = 0;
 
   let index = 0;
@@ -85,14 +83,17 @@ function updatePiles(piles) {
     if (pile === 0) {
       continue;
     }
+
     newPile++;
     piles[index] = --pile;
     index++;
   }
+
   piles.push(newPile);
 }
 
 function arrayHasDuplicateArrays(arr) {
+  // Gör om varje fält i fältet till en sträng så att man kan jämföra
   let sortedArr = arr.map((a) => a.sort().reverse().join(","));
   let sortedSet = new Set(sortedArr);
   return sortedSet.size !== sortedArr.length;
@@ -101,34 +102,26 @@ function arrayHasDuplicateArrays(arr) {
 function play() {
   let rounds = 0;
   let piles = createPiles();
-  createPileContainers(sortPiles(piles));
-  for (let i of sortPiles(piles)) {
-    console.log(i);
-  }
+  piles = sortPiles(piles);
+  createPileContainers(piles);
 
   let pileArchive = new Array();
-  pileArchive.push(sortPiles(piles));
-
+  pileArchive.push(piles);
+  
   newPilesBtn.addEventListener("click", function () {
     rounds++;
     pilesContainer.innerHTML = "";
-    console.log("New: ");
+  
     updatePiles(piles);
-    pileArchive.push(sortPiles(piles));
-    createPileContainers(sortPiles(piles));
-    for (let i of sortPiles(piles)) {
-      console.log(i);
-    }
-    // console.log(pileArchive);
-    if (arrayHasDuplicateArrays(pileArchive)) {
-      console.log("Duplicate found");
+    piles = sortPiles(piles);
+    pileArchive.push(piles);
 
-      console.log("Rounds: " + rounds);
+    createPileContainers(piles);
+  
+    if (arrayHasDuplicateArrays(pileArchive)) {
       if (pileArchive.at(-1).join(",") === pileArchive.at(-2).join(",")) {
-        console.log("Last two piles are equal");
         alert(`Patiensen har gått ur på runda ${rounds}`);
       } else {
-        console.log("The deck has looped");
         alert(`Högarna har loopat på runda ${rounds}`);
       }
 
